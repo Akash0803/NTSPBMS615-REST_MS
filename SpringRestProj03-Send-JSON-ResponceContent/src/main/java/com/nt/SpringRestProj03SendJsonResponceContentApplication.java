ebb992d8570540b4372a108e996238ebf11efaec
Akash0803 <aakashmr14343@gmail.com>
2022-07-02 15:31:55 +0530

    PathVariable

---
+++ SpringRestProj03-Send-JSON-ResponceContent/src/main/java/com/nt/SpringRestProj03SendJsonResponceContentApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestProj03SendJsonResponceContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestProj03SendJsonResponceContentApplication.class, args);
+	}
+
+}
