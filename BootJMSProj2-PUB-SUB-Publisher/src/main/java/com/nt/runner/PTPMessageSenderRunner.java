1977468b39d07ca3b8300a30e8c8622a70e3db1b
Akash0803 <aakashmr14343@gmail.com>
2022-09-07 16:21:11 +0530

    JMS-PUB-SUB Model Sending Message using ActiveMQ

---
+++ BootJMSProj2-PUB-SUB-Publisher/src/main/java/com/nt/runner/PTPMessageSenderRunner.java
@@ -0,0 +1,42 @@
+package com.nt.runner;
+
+import java.util.Date;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Component;
+@Component
+public class PTPMessageSenderRunner implements CommandLineRunner {
+	@Autowired
+	private JmsTemplate template;
+	@Override
+	public void run(String... args) throws Exception {
+	/*	//Normal Anonymous Inner Class for MessageCreator()
+		template.send("queue1",new MessageCreator() {
+			
+			@Override
+			public Message createMessage(Session session) throws JMSException {
+				Message message=session.createTextMessage("From Sender "+new Date());
+				return message;
+			}
+		}); */
+		
+		
+		/*//LAMBDA Based Anonymous Inner Class for MessageCreator()
+		template.send("queue1",ses->{
+			Message message=ses.createTextMessage("From Sender :: "+new Date());
+			return message;
+		}); */
+		
+		//LAMBDA Based Anonymous Inner Class for MessageCreator()
+				template.send("topic1",ses->ses.createTextMessage("From Sender :: "+new Date()));
+		System.out.println("From Publisher ---->  Msg is Sent ");
+	}
+
+}
